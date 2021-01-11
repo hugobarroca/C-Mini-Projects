1b38f41c854d2b0fb17c2c89de40851f5f60f73b
Hugo Barroca <hugobarroca98@gmail.com>
2021-01-11 21:39:32 +0000

    Removed booleans, added prints, fixed some equal signs setting variables instead of comparing values.

--- C/Bubblesort/main.c
+++ C/Bubblesort/main.c
@@ -1,16 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <stdbool.h>
 #include "usefulprints.h"
 
 int* sort(int* unsortedArray, int size){
-	bool isOrdered = 0;
-	while(isOrdered){
-		int currentCounter = false;
-		int tempInt;
-		for(int i = 0; i < size - 1; ++i){
-			currentCounter = 0;
-			if(i + 1 < size - 1){
+	printf("Starting to sort array!\n");
+	int isOrdered = 0;
+	int currentCounter;
+	int tempInt;
+
+	while(isOrdered == 0){
+		printf("Sorting... ");
+		printArray(unsortedArray, size);
+		currentCounter = 0;
+		for(int i = 0; i < size; ++i){
+			if(i + 1 < size){
 				if(unsortedArray[i] > unsortedArray[i + 1]){
 					tempInt = unsortedArray[i];
 					unsortedArray[i] = unsortedArray[i+1];
@@ -21,21 +24,18 @@ int* sort(int* unsortedArray, int size){
 			}
 		}
 		if (currentCounter == 0){
-			isOrdered = true;
+			isOrdered = 1;
 		}
 	}
 }
 
 int main(){
-	int array[] = {4, 2, 43};
+	int array[] = {4, 2, 43, 54, 2, 43, 65, 7};
 	int size = sizeof(array) / sizeof(array[0]);
 	printf("The current array is of size %d!\n", size);
 	printArray(array, size);
 	sort(array, size);
+	printArray(array, size);
 	return 0;
 }
-
-
-
-
 
